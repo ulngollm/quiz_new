b53871c92ff367c83c26e202669aef74c353a61a
ulngollm <ul@noknok.ru>
2020-07-29 17:51:02 +0300

    modify input:text and valudation of step, add mask phone, add toggle to success page

--- quiz.js
+++ quiz.js
@@ -11,8 +11,8 @@ function sendRequest() {
 
     xhr.onload = function () {
         console.log(xhr.response);
+        if(xhr.status==200) showSuccessPage();
     }
-    console.log(request);
 }
 
 
@@ -31,53 +31,50 @@ function addAnswer() {
     quizAnswers.push(newAnswer);
 }
 
-function editAnswer(step) {
-
-}
-
 function goFinalPage() {
-    for (container of document.querySelectorAll('.form'))
-        container.classList.toggle('form_hidden');
+    document.querySelector('.form_answers').classList.add('form_hidden');
+    document.querySelector('.form_result').classList.remove('form_hidden');
+}
+function showSuccessPage(){
+    document.querySelector('.form_result').classList.add('form_hidden');
+    document.querySelector('.form_success').classList.remove('form_hidden');
 }
 
 function initQuiz() {
+    showError(false)
     let step = quiz[currentStep];
     document.querySelector('.form__header').innerText = step['question'];
     document.querySelector('.form__body').innerHTML = '';
-    showError(false);
     for (answer in step['answers']) {
         let elem = `<label class="control"><input type="radio" class="control__input" name="choose" value="${answer}" required><span class="control__label control__label_radio">${answer}</span></label>`;
         document.querySelector('.form__body').insertAdjacentHTML('beforeend', elem);
     }
     if (step['textField']) {
-        let input = '<input class="input" type="text" name="other" placeholder="Другое">';
+        let input = '<label class="control"><input type="radio" class="control__input control__input_replacement" name="choose" value="" required><input class="input" type="text" name="other" placeholder="Другое"></label>';
         document.querySelector('.form__body').insertAdjacentHTML('beforeend', input);
     }
 }
 
-function getInput() {
-
-}
-function checkValid(hasTextField){
-    if(hasTextField) return document.querySelector('input:checked') || document.querySelector('input[name="other"]').value || null;
-    else return document.querySelector('input:checked') || null;
+function checkValid(){
+    let hasAnswer = document.querySelector('form.form__body').checkValidity();
+    hasAnswer? showError(false): showError(true);
+    return hasAnswer;
 }
 
 function nextStep() {
-    let answer = checkValid(quiz[currentStep]['textField']);
-    if (answer) {
-        answer = answer.value;
+    if (checkValid()) {
+        let answer = document.querySelector('input[type="radio"]:checked').value;
         addAnswer();
         currentStep = quiz[currentStep]['answers'][answer] || quiz[currentStep]['textField'];
         if (currentStep != "end") initQuiz();
         else goFinalPage();
     }
-    else showError(true);
+
 }
 
 function showError(isError){
     let err = document.querySelector('.form__error');
-    isError? err.classList.add('form__error_active'):err.classList.remove('form__error_active');
+    isError? err.classList.add('form__error_active') : err.classList.remove('form__error_active');
 }
 
 function prevStep() {
@@ -95,8 +92,17 @@ initQuiz();
 document.querySelector('.form__button_next').addEventListener('click', nextStep);
 document.querySelector('.form__button_prev').addEventListener('click', prevStep);
 document.querySelector('input[name="other"]').addEventListener('focus', function(){
-    document.querySelector('form.form__body').reset();
+    document.querySelector('.control__input_replacement').checked = true;
 })
+document.querySelector('input[name="other"]').addEventListener('input', function(e){
+    document.querySelector('.control__input_replacement').value = e.currentTarget.value;
+})
+document.querySelector('form.form__body').addEventListener('change', checkValid);
+
+var phoneMask = IMask(
+    document.querySelector('.input_icon-phone'), {
+    mask: '+{7} (000) 000-00-00'
+});
 
 var form = document.querySelector('form.form__feedback');
 form.addEventListener('submit', function (e) {
@@ -107,8 +113,6 @@ form.addEventListener('submit', function (e) {
         ans = new QuizAnswer(name, value)
         quizAnswers.push(ans);
     }
-    console.log(formData);
-    console.log(JSON.stringify(formData));
     sendRequest();
 
 })
