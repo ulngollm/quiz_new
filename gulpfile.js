5146ee41223ed42270be40e051f61bfc9252f325
ulngollm <ulngollm@gmail.com>
2021-04-02 14:05:12 +0300

    доделана сборка

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,39 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, parallel, watch } = require("gulp");
 const concat = require('gulp-concat');
-// const browserSync = require("browser-sync").create();
-// var reload = browserSync.reload;
+const browserSync = require("browser-sync").create();
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
-const prepare = function(){
+const js = function(){
     return src('src/js/*.js')
     .pipe(concat('main.min.js'))
     .pipe(dest('build'));
 }
-exports.prepare = series(prepare);
+const docs = function(){
+    return src('src/docs/*')
+    .pipe(dest('build'));
+}
+const styles = function(){
+    return src('src/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('build'));
+}
+const watcher = function(){
+    watch('src/**/*.js', js);
+    watch('src/docs/**',docs);
+    watch('src/scss/**',styles);
+
+    watch('build/**', { delay: 1000 }, browserSync.reload);
+
+}
+const server = function(){
+    browserSync.init({
+        server: {
+            baseDir: "./build/"
+        }
+    });
+}
+exports.server = parallel(server, watcher);
+exports.prepare = series(js);
+exports.styles = series(styles);
+exports.build = parallel(js, docs, styles);
